4f3d354698ea4da0c552139f75ee6d0a62f4657e
[suyash patel] <suyashpatelmrj@gmail.com>
2021-06-17 21:16:14 +0530

    Added desigh

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ class NameForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = { value: '' };
-
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -24,57 +23,32 @@ class NameForm extends React.Component {
 
     render() {
         return ( 
-        <form onSubmit = { this.handleSubmit } >
-          <div class="px-12 pb-10">
-          <div class="w-full mb-2">
-            <div class="flex justify-center">
-           <input class="px-8  w-full border rounded py-2 text-gray-700 focus:outline-none items-center"  type = "submit"
-        value = {"Hello World" }
-        /> 
-        </div>
-        </div>
-        <div class="w-full mb-2">
-            <div class="flex justify-center">
-        <input type = "submit" class="px-8  w-full border rounded py-2 text-gray-700 focus:outline-none items-center"
-        value = {"ab192agjka8na71ka" }
-        /> 
-        </div>
-        </div>
-           <br />
-           <div class="w-full mb-2">
-            <div class="flex justify-center">
-        
-            Chip:
-            <input type = "text" class="px-3  w-full border rounded py-2 text-gray-700 focus:outline-none items-center"
-            placeholder="Chip to withdraw"
-            value = { this.state.value }
-            onChange = { this.handleChange }
-            />  
-            </div>
-            </div>
-            <br />
-              {/* now this will show the usd*/}
-              <div class="w-full mb-2">
-            <div class="flex justify-center">
-              USD:
+       <div class="flex w-full min-h-screen justify-center items-center">
+         <div class="border-2 rounded-lg  border-black bg-gray-200 shadow-2xl">
+           <form onSubmit = { this.handleSubmit } class="">
+              <div class="m-4">Enter Chip:</div>
+              <input type ="text" class="mx-8 border rounded-lg  border-black "
+              placeholder="Chip to withdraw"
+              value = { this.state.value }
+              onChange = { this.handleChange }
+              />
               <br />
-         <input type = "text" class="px-3  w-full border rounded py-2 text-gray-700 focus:outline-none items-center"
-        value = {"$"+((this.state.value)*10) }
-        /> 
-        </div>
-        </div>
-           <br />
-           <div class="w-full mb-2">
-            <div class="flex justify-center">
-        <input class="px-8  w-full border rounded py-2 text-gray-700 focus:outline-none items-center" type = "submit" value = "withdraw" / >
-          </div>
-          </div>
-           </div>
-            </form>
+              {/* now this will show the usd*/}
+              <div class="m-4">Your USD:</div>
+              <input type = "text" class="mx-8 border rounded-lg  border-black "
+              value = {"$"+((this.state.value)*10) }
+              />
+              <br />
+              <div class="my-8 grid justify-items-center">
+                 <input type="submit" value="Withdraw" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2  rounded-full text-center" />
+               </div>          
+           </form>
+         </div>
+       </div>
         );
     }
 }
-    
+
 
 ReactDOM.render( <
     NameForm / > ,
