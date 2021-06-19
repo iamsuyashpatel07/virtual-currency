f3c1af9c273f8b0acf88e13212636b721fdba6f3
[suyash patel] <suyashpatelmrj@gmail.com>
2021-06-20 02:49:53 +0530

    used to fix custom input box + padding + margin

--- src/index.js
+++ src/index.js
@@ -23,28 +23,69 @@ class NameForm extends React.Component {
 
     render() {
         return ( 
-       <div class="flex w-full min-h-screen justify-center items-center">
-         <div class="border-2 rounded-lg  border-black bg-gray-200 shadow-2xl">
-           <form onSubmit = { this.handleSubmit } class="">
-              <div class="m-4">Enter Chip:</div>
-              <input type ="text" class="mx-8 border rounded-lg  border-black "
+          <div class="min-h-screen flex items-center bg-purple-500">
+          <div class="flex-1 mx-auto max-w-3xl p-10  border-2 rounded-lg  border-black shadow-2xl  bg-gray-200 shadow-2xl">
+             <form onSubmit = { this.handleSubmit } className="">
+              <ul class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-8">
+                  <li class="">
+  
+                  </li>
+                  <li class="">
+  
+                  </li>
+                  <li class="flex">
+                     
+                      <div class="border-2 border-black">
+                          <div class="float-left">
+                              <b class="pr-4">Settings</b>
+                          </div>
+                          <div class="float-right">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            </svg>
+                          </div>
+                        
+                      </div>
+                      
+                  </li>
+                  <li class="">
+                      Enter Chip:
+                  </li>
+                  <li class="">
+                      Enter USD:
+                  </li>
+                  <li class="">
+  
+                  </li>
+                  <li class="">
+                  <input type ="text" className="border rounded-lg  border-black "
               placeholder="Chip to withdraw"
               value = { this.state.value }
               onChange = { this.handleChange }
               />
-              <br />
-              {/* now this will show the usd*/}
-              <div class="m-4">Your USD:</div>
-              <input type = "text" class="mx-8 border rounded-lg  border-black "
+                  </li>
+                  <li class="">
+                  <input type = "text" className="border rounded-lg  border-black "
               value = {"$"+((this.state.value)*10) }
               />
-              <br />
-              <div class="my-8 grid justify-items-center">
-                 <input type="submit" value="Withdraw" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2  rounded-full text-center" />
-               </div>          
-           </form>
-         </div>
-       </div>
+                  </li>
+                  <li class="">
+                     
+                  </li>
+                  <li class="">
+                  <input type="submit" value="Withdraw" className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2  rounded-full text-center" />
+                  </li>
+                  <li class="">
+                  <input type="submit" value="Cancel" className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2  rounded-full text-center" />
+                  </li>
+                  <li class="">
+                     
+                  </li>
+              </ul>
+              </form>
+          </div>
+      </div>
         );
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            gridAutoColumns: ['hover', 'focus'],
+            gridAutoColumns: false,
+            gridAutoColumns: {
+                '2fr': 'minmax(0, 2fr)',
+            }
+        },
     },
     variants: {
         extend: {},
